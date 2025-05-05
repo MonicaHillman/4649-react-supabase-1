40c96b0e1c6b65111e062101f0e527abf65c70f4
monica hillman <mmhillman1@ucs.br>
2025-05-05 11:22:02 -0300

    aula 03 fix

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://llapqibssiongylqsdgq.supabase.co";
-const chaveApi =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsYXBxaWJzc2lvbmd5bHFzZGdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODEyNDcsImV4cCI6MjA2MTM1NzI0N30.WDGHxmJEDZOD6uaxn1w-whhMd30X07XhHigrBqZRPVU";
+const supabaseUrl = "COLOQUE SUA SUPABASE URL";
+const chaveApi = "COLOQUE A SUA CHAVE AQUI";
 
 export const supabase = createClient(supabaseUrl, chaveApi);
